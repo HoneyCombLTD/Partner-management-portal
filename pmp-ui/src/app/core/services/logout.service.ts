278e5a68f341c18b925f60ba91001b6aa9c502c8
Obi-Wan Kadogo <prox0591@gmail.com>
2022-09-17 12:41:43 +0300

    update logout service

--- pmp-ui/src/app/core/services/logout.service.ts
+++ pmp-ui/src/app/core/services/logout.service.ts
@@ -1,17 +1,17 @@
-import { LoginRedirectService } from './loginredirect.service';
-import { Router } from '@angular/router';
-import { ResponseModel } from './../models/response.model';
-import { LogoutResponse } from './../models/logoutresponse';
-import { Injectable } from '@angular/core';
+import { LoginRedirectService } from "./loginredirect.service";
+import { Router } from "@angular/router";
+import { ResponseModel } from "./../models/response.model";
+import { LogoutResponse } from "./../models/logoutresponse";
+import { Injectable } from "@angular/core";
 import {
   HttpClient,
   HttpResponse,
-  HttpErrorResponse
-} from '@angular/common/http';
-import { AppConfigService } from 'src/app/app-config.service';
+  HttpErrorResponse,
+} from "@angular/common/http";
+import { AppConfigService } from "src/app/app-config.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class LogoutService {
   constructor(
@@ -22,27 +22,8 @@ export class LogoutService {
   ) {}
 
   logout() {
-    window.location.href = `${this.appService.getConfig().baseUrl}v1/partnermanager/logout/user?redirecturi=`+btoa(window.location.href);
-
-    /*
-    this.http
-      .delete(`${this.appService.getConfig().baseUrl}v1/partnermanager/logout/user`, {
-        observe: 'response'
-      })
-      .subscribe(
-        (res: HttpResponse<ResponseModel<LogoutResponse>>) => {
-          if (res.body.response.status === 'Success') {
-            this.redirectService.redirect(
-              window.location.origin + '/pmp-ui/'
-            );
-          } else {
-            window.alert(res.body.response.message);
-          }
-        },
-        (error: HttpErrorResponse) => {
-          window.alert(error.message);
-        }
-      );
-    */
+    window.location.href = `${this.appService.getConfig().baseUrl}${
+      this.appService.getConfig().logout
+    }?redirecturi=${btoa(window.location.href)}`;
   }
 }
