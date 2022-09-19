dd03bec2f5e215eda8fd9e76d9c5ab6da9d8ef2a
Obi-Wan Kadogo <prox0591@gmail.com>
2022-09-19 14:27:29 +0300

    login and logout service

--- pmp-ui/src/app/core/services/loginredirect.service.ts
+++ pmp-ui/src/app/core/services/loginredirect.service.ts
@@ -22,13 +22,20 @@ export class LoginRedirectService {
     //     this.appService.getConfig().login
     //   }` + btoa(url);
 
+    // const stateParam = uuid();
+    // /*this.cookie.set('state', "");
+    // this.cookie.set('state', stateParam, undefined, '/');*/
+    // window.location.href =
+    //   `${this.appService.getConfig().baseUrl}partnermanager/login/` +
+    //   btoa(url) +
+    //   "?state=" +
+    //   stateParam;
+
     const stateParam = uuid();
-    /*this.cookie.set('state', "");
-    this.cookie.set('state', stateParam, undefined, '/');*/
+    this.cookie.set("state", stateParam, undefined, "/");
     window.location.href =
-      `${this.appService.getConfig().baseUrl}v1/partnermanager/login/` +
-      btoa(url) +
-      "?state=" +
-      stateParam;
+      `${this.appService.getConfig().baseUrl}${
+        this.appService.getConfig().login
+      }` + btoa(url);
   }
 }
