5f3cc72b8bab6ab5d4ffe01096b86cba860fc798
Obi-Wan Kadogo <prox0591@gmail.com>
2022-09-19 10:56:07 +0300

    fix login redirect

--- pmp-ui/src/app/core/services/loginredirect.service.ts
+++ pmp-ui/src/app/core/services/loginredirect.service.ts
@@ -1,17 +1,34 @@
-import { Injectable } from '@angular/core';
-import { v4 as uuid } from 'uuid';
-import { CookieService } from 'ngx-cookie-service';
-import { AppConfigService } from 'src/app/app-config.service';
-
+import { Injectable } from "@angular/core";
+import { v4 as uuid } from "uuid";
+import { CookieService } from "ngx-cookie-service";
+import { AppConfigService } from "src/app/app-config.service";
 
 @Injectable()
 export class LoginRedirectService {
-
-  constructor(private cookie: CookieService, private appService: AppConfigService) { }
+  constructor(
+    private cookie: CookieService,
+    private appService: AppConfigService
+  ) {}
 
   redirect(url: string) {
+    // const stateParam = uuid();
+    // this.cookie.set('state', stateParam, undefined, '/');
+    // window.location.href = `${this.appService.getConfig().baseUrl}v1/partnermanager/login/` + btoa(url);
+
     const stateParam = uuid();
-    this.cookie.set('state', stateParam, undefined, '/');
-    window.location.href = `${this.appService.getConfig().baseUrl}v1/partnermanager/login/` + btoa(url);
+    /*this.cookie.set('state', "");
+    this.cookie.set('state', stateParam, undefined, '/');*/
+    window.location.href =
+      `${this.appService.getConfig().baseUrl}v1/partnermanager/login/` +
+      btoa(url) +
+      "?state=" +
+      stateParam;
+
+    // const stateParam = uuid();
+    // this.cookie.set("state", stateParam, undefined, "/");
+    // window.location.href =
+    //   `${this.appService.getConfig().baseUrl}${
+    //     this.appService.getConfig().login
+    //   }` + btoa(url);
   }
 }
